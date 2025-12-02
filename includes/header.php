91b3ce4d5a812e0b25e151f7d779015b43252773
JhxssxR <j.antukan.549054@umindanao.edu.ph>
2025-12-02 20:50:25 +0800

    clickable logo

--- includes/header.php
+++ includes/header.php
@@ -11,10 +11,10 @@ $base = '/PhotographyReservationSystem/';
 
 <header class="topbar">
   <div class="wrap">
-    <div class="brand">
+    <a href="<?= $base ?>index.php" class="brand" style="text-decoration:none;color:inherit;">
       <div class="logo"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/><circle cx="12" cy="13" r="4"/></svg></div>
       <span class="brand-name">PhotoReserve</span>
-    </div>
+    </a>
     <nav class="nav">
       <?php $cur = basename(parse_url(
         ($_SERVER['REQUEST_URI'] ?? ''), PHP_URL_PATH)); ?>
